87186841083f192f082277976c46c5934a0da3cc
Alexandra Vystavkina <a.vystavkina@yandex.ru>
2019-11-07 15:40:54 +0300

    first commit Extended-Syntax

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -1,4 +1,5 @@
 
+'use strict'
 
 function calculateQuadraticEquation(){
     let a = +window.a.value;
@@ -12,7 +13,16 @@ function calculateQuadraticEquation(){
 
 function getResult(a,b,c){
     // код для задачи №1 писать здесь
-    //return x;
+    let discriminant = b**2 - 4*a*c;
+    let x = [];
+    if (discriminant < 0) {
+      return x;
+    } else if(discriminant == 0) {
+      x = [-b / 2*a];
+    } else if(discriminant > 0) {
+      x = [(-b + discriminant)/ 2*a, (-b - discriminant)/ 2*a];
+    }
+    return x;
 }
 
 function calculateAverageRating(){
@@ -23,7 +33,17 @@ function calculateAverageRating(){
 
 function getAverageMark(marks){
     // код для задачи №2 писать здесь
-    //return averageMark;
+    let sum = 0;
+    let averageMark = [];
+    for (i = 0; i <= averageMark.length; i++) {
+    	if(averageMark.length > 4) {
+           averageMark.splice(5);
+           console.log(averageMark);
+    	} 
+        sum += averageMark[i];
+        let arithmeticMean = sum / averageMark.length;
+    }
+    return averageMark;
 }
 
 function calculateDrinkTask(){
@@ -35,6 +55,9 @@ function calculateDrinkTask(){
 
 function askDrink(name,dateOfBirthday){
     // код для задачи №3 писать здесь
-    //console.log(result)
-    //return result;
+    let year = new Date().getFullYear();
+    let result = (year >= 2001) ? `Сожалею, ${name}, но я не могу вам продать алкоголь. 
+    Зато могу предложить вам замечательный клюквенный компот!` : `Не желаете ли олд-фэшн, ${name}?`;
+    return result;
+    console.log(result);
 }
